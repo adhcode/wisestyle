f6070ec1f5c4df8ab2c6aac98776f4eb55ec494e
ADHHHH <adh.devv@gmail.com>
2025-05-25 00:00:30 +0100

    fix: remove keepAlive option to resolve type errors

--- server/src/redis/redis.service.ts
+++ server/src/redis/redis.service.ts
@@ -33,7 +33,6 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
                     return delay;
                 },
                 connectTimeout: 20000, // 20 seconds
-                keepAlive: 30000, // 30 seconds keepalive
                 noDelay: true
             },
             disableOfflineQueue: false,
@@ -156,7 +155,6 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
                         return Math.min(retries * 2000, 10000);
                     },
                     connectTimeout: 20000,
-                    keepAlive: 30000, // 30 seconds keepalive
                     noDelay: true
                 },
                 disableOfflineQueue: false,
