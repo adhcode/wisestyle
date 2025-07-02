7371b52b03cd30dc537a8cc5a1bbdf0f2c4e8202
ADHHHH <adh.devv@gmail.com>
2025-07-02 15:17:47 +0100

    newwwww

--- server/start.sh
+++ server/start.sh
@@ -5,14 +5,5 @@ echo "Current user: $(whoami)"
 echo "Working dir: $(pwd)"
 ls -al
 
-echo "Running database migrations..."
-npx prisma migrate deploy
-
-# Optionally seed database on first deploy
-if [ "$SEED_DB" = "true" ]; then
-  echo "Seeding database..."
-  npx prisma db seed
-fi
-
 echo "Starting application..."
 exec node dist/main 
